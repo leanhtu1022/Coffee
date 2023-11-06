69627cab153ef7d51e234a06aeb478949a0c7a8c
leanhtu1022 <144982818+leanhtu1022@users.noreply.github.com>
2023-11-06 09:41:10 +0700

    Delete src/components directory

--- src/components/ChangePassword/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const ProfileContainer = styled.div`
-
-  padding: 1rem 4rem;
-  max-width: 80%;
-  margin: auto;
-  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
-`;
